e6cd854bbe73c2978a5eb31fda63ebe805a9bcf2
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-04-29 15:54:35 +0100

    Add initial version
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ create-pdf/handler.go
@@ -0,0 +1,45 @@
+package function
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+
+	"github.com/phpdave11/gofpdf"
+)
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+	msg := r.URL.Query().Get("msg")
+	if msg == "" {
+		http.Error(w, "give a ?msg= value for your PDF", http.StatusBadRequest)
+		return
+	}
+
+	dir := os.TempDir()
+
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 16)
+	pdf.Cell(40, 10, msg)
+
+	pdfPath := path.Join(dir, "hello.pdf")
+	err := pdf.OutputFileAndClose(pdfPath)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res, err := ioutil.ReadFile(pdfPath)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/octet-stream")
+	w.Header().Set("Content-Disposition", `attachment; filename="content.pdf"`)
+
+	w.WriteHeader(http.StatusOK)
+
+	w.Write(res)
+}
